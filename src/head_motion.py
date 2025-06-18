fix rotation

src/head_motion.py
@@ -2,7 +2,7 @@
 import rospy 
 from nav_msgs.msg import Path, Odometry
 from geometry_msgs.msg import Pose2D
-from cohan_msgs.msg import TrajectoryStamped
+from cohan_msgs.msg import Trajectory
 import tf
 import numpy as np
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
@@ -18,8 +18,8 @@ class head_motion:
         self.head_pose_time = 2.0 #in seconds (towards whose goal, the head is oriented)
         self.headtj_pub = rospy.Publisher('/head_traj_controller/command' , JointTrajectory , queue_size=10)
         rospy.Subscriber('/base_pose_ground_truth' , Odometry , self.robot_pose_cb)
-        rospy.Subscriber('/move_base/HATebLocalPlannerROS/local_traj' , TrajectoryStamped , self.angle_calculator)
-
+        rospy.Subscriber('/move_base/HATebLocalPlannerROS/local_traj' , Trajectory , self.angle_calculator)
+        # rospy.Timer(rospy.Duration(0.1), self.timer_callback)
     def robot_pose_cb(self, data):
         q = (
             data.pose.pose.orientation.x,
@@ -34,19 +34,29 @@ class head_motion:
         self.robot_pose.y = data.pose.pose.position.y
         self.robot_pose.theta = tf.transformations.euler_from_matrix(m)[2]
 
+    def timer_callback(self, event):
+        self.head_joint_trajectory_goal = JointTrajectory()
+        self.head_goal_point = JointTrajectoryPoint()
+        self.head_joint_trajectory_goal.joint_names = ['head_pan_joint' , 'head_tilt_joint']
+        self.head_goal_point.positions = [-3 , 0.0]
+        self.head_goal_point.time_from_start = rospy.Duration(0.1)
+        self.head_joint_trajectory_goal.points.append(self.head_goal_point) 
+        self.headtj_pub.publish(self.head_joint_trajectory_goal)
+        rospy.loginfo("Head Command Sent")
+
     def angle_calculator(self, data):
         self.head_joint_trajectory_goal = JointTrajectory()
         self.head_goal_point = JointTrajectoryPoint()
         self.head_joint_trajectory_goal.joint_names = ['head_pan_joint' , 'head_tilt_joint']
         for data_ in data.points:
             if data_.time_from_start > rospy.Duration(self.head_pose_time + 1): # Trial and Fix
-                self.point.x = data_.pose.position.x
-                self.point.y = data_.pose.position.y
+                self.point.x = data_.transform.translation.x
+                self.point.y = data_.transform.translation.y
                 q = (
-                data_.pose.orientation.x,
-                data_.pose.orientation.y,
-                data_.pose.orientation.z,
-                data_.pose.orientation.w
+                data_.transform.rotation.x,
+                data_.transform.rotation.y,
+                data_.transform.rotation.z,
+                data_.transform.rotation.w
                 )
 
                 m = tf.transformations.quaternion_matrix(q)
@@ -58,13 +68,14 @@ class head_motion:
                 # break
         point_pos = np.array([self.point.x , self.point.y])
         robot_pos = np.array([self.robot_pose.x , self.robot_pose.y])
-        # print(person_pos)
         vector_to_goal = point_pos - robot_pos
-        goal_orientation = (math.atan2(vector_to_goal[1], vector_to_goal[0]))- self.robot_pose.theta
-        print(goal_orientation)
-        self.head_goal_point.positions = [goal_orientation , 0.0]
-        self.head_goal_point.time_from_start = rospy.Duration(2.0)
-        self.head_joint_trajectory_goal.points.append(self.head_goal_point) 
+        goal_yaw = math.atan2(vector_to_goal[1], vector_to_goal[0])
+        goal_orientation = (goal_yaw - self.robot_pose.theta + math.pi) % (2 * math.pi) - math.pi 
+        
+        self.head_goal_point.positions = [goal_orientation, 0.0]
+        self.head_goal_point.time_from_start = rospy.Duration(0.1)
+        self.head_joint_trajectory_goal.points.append(self.head_goal_point)
+        self.head_joint_trajectory_goal.header.stamp = rospy.Time.now()
         self.headtj_pub.publish(self.head_joint_trajectory_goal)
         rospy.loginfo("Head Command Sent")
         # Calculate the distance from the robot to the person